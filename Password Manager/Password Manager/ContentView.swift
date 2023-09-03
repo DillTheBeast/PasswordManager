18324235e2dce84062dea9491bc5b94becf221ad
DillTheBeast <dillonmaltese@gmail.com>
2023-09-02 23:11:13 -0400

    a

--- Password Manager/Password Manager/ContentView.swift
+++ Password Manager/Password Manager/ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var addAccountScene = false
-    @State private var viewAccountScene = false
+    @State private var addAccountScene: Bool = false
+    @State private var viewAccountScene: Bool = false
     @State private var viewInfoScene: Bool = false
-    @State private var savedWords: [String] = []
+    @State private var deleteAccountScene: Bool = false
     
     @AppStorage("Accounts") var AccountData: Data = Data()
     @AppStorage("Usernames") var UsernameData: Data = Data()
@@ -13,6 +13,10 @@ struct ContentView: View {
     @State private var Account: [String] = []
     @State private var Username: [String] = []
     @State private var Password: [String] = []
+    
+    @State private var accountSelect: String = ""
+    @State private var usernameSelect: String = ""
+    @State private var passwordSelect: String = ""
     
     let skyBlue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
     
@@ -23,13 +27,17 @@ struct ContentView: View {
                 if addAccountScene {
                     AddAccount(addAccountScene: $addAccountScene, accounts: $Account, usernames: $Username, passwords: $Password)
                 } else if viewAccountScene {
-                    ViewAccount(viewAccountScene: $viewAccountScene, viewInfoScene: $viewInfoScene, accounts: $Account)
+                    ViewAccount(viewAccountScene: $viewAccountScene, viewInfoScene: $viewInfoScene, accounts: $Account, usernames: $Username, passwords: $Password, accountSelect: $accountSelect, usernameSelect: $usernameSelect, passwordSelect: $passwordSelect)
                 } else if viewInfoScene {
-                    
+                    ViewInfo(viewInfoScene: $viewInfoScene, accountSelect: $accountSelect, usernameSelect: $usernameSelect, passwordSelect: $passwordSelect)
+                } else if deleteAccountScene {
+                    deleteAccount(accounts: $Account, usernames: $Username, passwords: $Password, deleteAccountScene: $deleteAccountScene)
                 } else {
                     Button("Add an Account") {
                         addAccountScene.toggle()
                         viewAccountScene = false
+                        viewInfoScene = false
+                        deleteAccountScene = false
                     }
                     .padding()
                     .border(Color.white, width: 1)
@@ -38,6 +46,18 @@ struct ContentView: View {
                     Button("View your Accounts") {
                         viewAccountScene.toggle()
                         addAccountScene = false
+                        viewInfoScene = false
+                        deleteAccountScene = false
+                    }
+                    .padding()
+                    .border(Color.white, width: 1)
+                    .foregroundColor(.white)
+                    
+                    Button("Delete an Account") {
+                        deleteAccountScene.toggle()
+                        addAccountScene = false
+                        viewAccountScene = false
+                        viewInfoScene = false
                     }
                     .padding()
                     .border(Color.white, width: 1)
@@ -162,11 +182,51 @@ struct AddAccount: View {
     }
 }
 
+struct deleteAccount: View {
+    @Binding var accounts: [String]
+    @Binding var usernames: [String]
+    @Binding var passwords: [String]
+    @Binding var deleteAccountScene: Bool
+    
+    var body: some View {
+        VStack() {
+            List(accounts.indices, id: \.self) { index in
+                Button(action: {
+                    accounts.remove(at: index)
+                    usernames.remove(at: index)
+                    passwords.remove(at: index)
+                    deleteAccountScene.toggle()
+                }) {
+                    Text(accounts[index])
+                        .foregroundColor(.white)  // set the text color to white
+                        .frame(maxWidth: .infinity, minHeight: 44)  // taking the full width and a minimum height
+                        .border(Color.black)
+                        .background(Color.black)  // set the row background to black
+                }
+            }
+            Button("Go Back") {
+                deleteAccountScene.toggle()
+            }
+            .padding()
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)  // Make the VStack occupy the entire space
+        .background(Color.black)
+    }
+}
 
 struct ViewAccount: View {
     @Binding var viewAccountScene: Bool
     @Binding var viewInfoScene: Bool
     @Binding var accounts: [String]
+    @Binding var usernames: [String]
+    @Binding var passwords: [String]
+    
+    @Binding var accountSelect: String
+    @Binding var usernameSelect: String
+    @Binding var passwordSelect: String
     
     var body: some View {
         VStack() {
@@ -174,6 +234,9 @@ struct ViewAccount: View {
             
             List(accounts.indices, id: \.self) { index in
                 Button(action: {
+                    accountSelect = accounts[index]
+                    usernameSelect = usernames[index]
+                    passwordSelect = passwords[index]
                     viewInfoScene = true  // Toggle to show the ViewInfo scene
                     viewAccountScene = false  // Close the ViewAccount scene
                 }) {
@@ -202,10 +265,30 @@ struct ViewAccount: View {
 
 struct ViewInfo: View {
     @Binding var viewInfoScene: Bool
+    @Binding var accountSelect: String
+    @Binding var usernameSelect: String
+    @Binding var passwordSelect: String
     
     var body: some View {
         VStack() {
-            Text("Username")
+            Text("Account: \(accountSelect)")
+                .foregroundColor(.white)  // set the text color to white
+                .frame(maxWidth: .infinity, minHeight: 44)  // taking the full width and a minimum height
+                .border(Color.black)
+                .background(Color.black)  // set the row background to black
+                .font(.title)
+            Text("Username: \(usernameSelect)")
+                .foregroundColor(.white)  // set the text color to white
+                .frame(maxWidth: .infinity, minHeight: 44)  // taking the full width and a minimum height
+                .border(Color.black)
+                .background(Color.black)  // set the row background to black
+                .font(.title)
+            Text("Password: \(passwordSelect)")
+                .foregroundColor(.white)  // set the text color to white
+                .frame(maxWidth: .infinity, minHeight: 44)  // taking the full width and a minimum height
+                .border(Color.black)
+                .background(Color.black)  // set the row background to black
+                .font(.title)
             
             Button("Go Back") {
                 viewInfoScene.toggle()
